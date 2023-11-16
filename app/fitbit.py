52c0f92543119706a9f1226ac3ab108c4bc47217
Gareth Stoyle <stoyleg@gmail.com>
2023-11-16 21:02:58 +0000

    added start of sleep data retrieval.

--- app/fitbit.py
+++ app/fitbit.py
@@ -4,8 +4,8 @@ import config
 
 ### Functionality to retrieve hr and acceleromoter data from fitbit API ###
 
-def get_hr_data(date, time_range=None):
-    hr_request = requests.get('https://api.fitbit.com/1/user/'+config.user_id+f'/activities/heart/date/{date}/1d/1min.json',
+def get_hr_data(date, time_range=['00:00', '23:59']):
+    hr_request = requests.get('https://api.fitbit.com/1/user/'+config.user_id+f'/activities/heart/date/{date}/1d/1min/time/{time_range[0]}/{time_range[1]}.json',
                                 headers={'Authorization': 'Bearer ' + config.access_token})
     if hr_request.status_code != 200:
         raise RuntimeError('fitbit api returned request status code:', hr_request.status_code)
@@ -14,7 +14,16 @@ def get_hr_data(date, time_range=None):
         pass
     return hr_data
 
-def get_accel_data(date, time_range='all'):
-    pass
+def get_sleep_data(date, time_range=['00:00', '23:59']):
+    hr_request = requests.get('https://api.fitbit.com/1/user/'+config.user_id+f'/sleep/date/{date}.json',
+                                headers={'Authorization': 'Bearer ' + config.access_token})
+    if hr_request.status_code != 200:
+        raise RuntimeError('fitbit api returned request status code:', hr_request.status_code)
+    print(hr_request.json())
+    sleep_data = ''
+    if time_range:
+        pass
+    return sleep_data
 
-hr_data = get_hr_data('2023-11-04')
+sleep_data = get_sleep_data('2023-11-04')
+print(sleep_data)
