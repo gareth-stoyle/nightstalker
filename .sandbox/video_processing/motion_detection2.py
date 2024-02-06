076779b916958b46619b174092b209b05995427d
Gareth Stoyle <stoyleg@gmail.com>
2024-02-06 16:12:49 +0000

    create motion detection algorithm and json db functionality

--- .sandbox/video_processing/motion_detection2.py
+++ .sandbox/video_processing/motion_detection2.py
@@ -1,39 +1,11 @@
 # https://pyimagesearch.com/2015/06/01/home-surveillance-and-motion-detection-with-the-raspberry-pi-python-and-opencv/
 # an averaged background solution
 # import the necessary packages
-import datetime
-# import imutils
 import cv2
 import time
 
-# conf 
-delta_thresh = 5
-min_area = 1000
-min_motion_frames = 8
-min_upload_seconds = 3.0
-show_video = True
-
-client = None
-
-# uploaded timestamp, and frame motion counter
-avg = None
-lastUploaded = datetime.datetime.now()
-motionCounter = 0
-frame_number = -1
-motion_frame_numbers = []
-
-# run from video dir
-video = cv2.VideoCapture("2024-02-04_footage.mp4")
-status = True
-
-while True:
-	status, frame = video.read()
-	frame_number += 1
-	if not status:
-		break
-	
-	timestamp = datetime.datetime.now()
-	text = "Unoccupied"
+def detect_motion(frame, min_area, delta_thresh, show_video, avg):
+	detected = False
 	# convert frame to grayscale, and blur it
 	# frame = imutils.resize(frame, width=500)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -43,7 +15,7 @@ while True:
 		print("[INFO] starting background model...")
 		avg = gray.copy().astype("float")
 		# rawCapture.truncate(0)
-		continue
+		return (False, avg)
 	# accumulate the weighted average between the current frame and
 	# previous frames, then compute the difference between the current
 	# frame and running average
@@ -58,7 +30,7 @@ while True:
 	
 	cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
-	# cnts = imutils.grab_contours(cnts)
+
 	# loop over the contours
 	for c in cnts:
 		# if the contour is too small, ignore it
@@ -68,46 +40,81 @@ while True:
 		# and update the text
 		(x, y, w, h) = cv2.boundingRect(c)
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-		text = "Occupied"
-	
-		
-		
-	# draw the text and timestamp on the frame
-	ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.35, (0, 0, 255), 1)
+		detected = True
 
-	# check to see if the room is occupied
-	if text == "Occupied":
-		motion_frame_numbers.append(frame_number)
-		# check to see if enough time has passed between uploads
-		if (timestamp - lastUploaded).seconds >= min_upload_seconds:
-			# increment the motion counter
-			motionCounter += 1
-			# check to see if the number of frames with consistent motion is
-			# high enough
-			if motionCounter >= min_motion_frames:
-				print('confident motion is detected.')
-				# update the last uploaded timestamp and reset the motion
-				# counter
-				lastUploaded = timestamp
-				motionCounter = 0
-	# otherwise, the room is not occupied
-	else:
-		motionCounter = 0
+	# draw the text and timestamp on the frame
+	# ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+	# cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+	# 	0.35, (0, 0, 255), 1)
 		
-	# check to see if the frames should be displayed to screen
 	if show_video:
 		# display the security feed
 		cv2.imshow("Feed", frame) # this won't work when ssh'd into pi
 		time.sleep(0.07)
 		key = cv2.waitKey(1) & 0xFF
-		# if the `q` key is pressed, break from the lop
+		# if the `q` key is pressed, break from the loop
 		if key == ord("q"):
-			break
+			exit()
 	# clear the stream in preparation for the next frame
 	# rawCapture.truncate(0)
+		
+	return (detected, avg)
+
+# motion detection config
+delta_thresh = 5
+min_area = 1000
+show_video = False
+avg = None
+# clip saving algo config
+motion_length = 0
+no_motion = 0
+min_motion_frames = 4
+min_clip_gap = 8 # 60s of no motion is enough to end clip and reset things
+frame_number = -1
+motion_frame_numbers = []
+writing_clip = False
+
+# run from video dir
+video = cv2.VideoCapture("2024-02-04_footage.mp4")
+status = True
+
+while True:
+	status, frame = video.read()
+	frame_number += 1
+	print(f'\n\n ### process frame {frame_number} ###\n')
+	if not status: # no more frames to parse
+		break
+	
+	detected, avg = detect_motion(frame, min_area, delta_thresh, show_video, avg)		
+
+	# functionality to handle clip creation for separate motion incidents
+	if detected:
+		motion_length += 1 # increment frames with motion
+		no_motion = 0 # reset frames with no motion
+		if motion_length >= min_motion_frames: # we should be recording clip
+			print('motion exceed min_motion_frames, writing')
+			writing_clip = True
+			# set writing_clip true
+			# write frames to video as well as previous x frames
+		motion_frame_numbers.append(frame_number)
+
+	else:
+		motion_length = 0 # reset frames with motion
+		no_motion += 1 # increment frames of no motion
+		if writing_clip:
+			if no_motion >= min_clip_gap: # no motion for over 60s, end clip (and shave 30s worth of frames)
+				print('we are writing but no_motion exceeds the min gap between clips, stop writing')
+				writing_clip = False
+			else: # continue writing
+				print("we are writing and haven't met the threshold to stop writing")
+		else:
+			print('no motion detected and we are not writing, continue')
+		# do nothing if not writing clip
+			
+	time.sleep(2)
+	print(f'motion length: {motion_length}')
+	print(f'writing clip: {writing_clip}')
+	print(f'no motion detected for: {no_motion} frames')
+		
 
 print("Frames with motion detected:", motion_frame_numbers)
